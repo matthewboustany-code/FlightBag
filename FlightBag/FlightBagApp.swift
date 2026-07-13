30fdf22e4308244be868128db58d3ccb0f6c73e5
MB <mb6gm@virginia.edu>
2026-07-12 23:12:52 -0400

    Initial Commit

---
+++ FlightBag/FlightBagApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightBagApp.swift
+//  FlightBag
+//
+//  Created by Hannah Purvis on 7/12/26.
+//
+
+import SwiftUI
+
+@main
+struct FlightBagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlightBagTests/FlightBagTests.swift
@@ -0,0 +1,18 @@
+//
+//  FlightBagTests.swift
+//  FlightBagTests
+//
+//  Created by Hannah Purvis on 7/12/26.
+//
+
+import Testing
+
+struct FlightBagTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
